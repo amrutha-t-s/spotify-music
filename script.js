8588850924526b0f7fc8719455cd178e2e16e2ac
amrutha-t-s <amrutha050605@gmail.com>
2026-03-10 23:55:00 +0530

    I have done a feture to skip the song and added ids to play other songs also

--- script.js
+++ script.js
@@ -23,4 +23,34 @@ audio.addEventListener("timeupdate", () => {
 progressBar.addEventListener("input", function () {
   let value = this.value;
   this.style.background = `linear-gradient(to right, #177200ff ${value}%, #333 ${value}%)`;
+  audio.currentTime = (progressBar.value * audio.duration) / 100;
+});
+
+songs = [
+  {
+    songName: "Song1",
+    songDes: "This is description for song1",
+    songImage: "image/1.jpg",
+  },
+];
+
+let playMusic = Array.from(document.getElementsByClassName("playMusic"));
+
+const makeAllPlay = () => {
+  playMusic.forEach((element) => {
+    element.classList.remove("fa-circle-pause");
+    element.classList.add("fa-circle-play");
+  });
+};
+
+playMusic.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    makeAllPlay();
+    e.target.classList.toggle("fa-circle-play");
+    e.target.classList.toggle("fa-circle-pause");
+
+    index = parseInt(e.target.id);
+    audio.src = `audio/${index}.weba`;
+    audio.currentTime = 0;
+  });
 });
